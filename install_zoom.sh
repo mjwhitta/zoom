e80d2cfce838c25561e3d9a955843af1b58f1a47
Miles Whittaker <mjwhitta@gmail.com>
2014-09-10 14:57:52 -0400

    Updated install script a little

--- install_zoom.sh
+++ install_zoom.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
+echo -n "Installing zoom..."
+
 # Save install location
 install_dir=$(pwd)
 
 # Make sure ~/bin exists and go to it
-mkdir -p ~/bin
-cd ~/bin
+mkdir -p ~/bin && cd ~/bin
 
 # Copy
-echo -n "Installing zoom..."
 cp $install_dir/zoom.rb z
 cp $install_dir/zoom_pager.sh .
 
-# Move to home dir and create zoomrc if needed
-cd
+# Create zoomrc if needed
 if [ ! -e .zoomrc ]; then
     $install_dir/zoom.rb --rc
 fi
+
 echo "done!"
