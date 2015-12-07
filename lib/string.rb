f122b851eb0ed44da519328566be67344002a5c7
Miles Whittaker <mjwhitta@gmail.com>
2015-12-06 21:43:13 -0500

    Updated string.rb

--- lib/string.rb
+++ lib/string.rb
@@ -1,15 +1,27 @@
 # Redefine String class to allow for colorizing, rsplit, and word wrap
 class String
+    def black
+        return colorize(30)
+    end
+
     def blue
-        return colorize(36)
+        return colorize(34)
     end
 
     def colorize(color)
         return "\e[#{color}m#{self}\e[0m"
     end
 
+    def cyan
+        return colorise(36)
+    end
+
     def green
         return colorize(32)
+    end
+
+    def purple
+        return colorize(35)
     end
 
     def red
@@ -28,5 +40,9 @@ class String
 
     def word_wrap(width = 70)
         return scan(/\S.{0,#{width}}\S(?=\s|$)|\S+/).join("\n")
+    end
+
+    def yellow
+        return colorize(33)
     end
 end
