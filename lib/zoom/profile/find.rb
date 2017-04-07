4a30f8c3ff2b9e4722ff6fd888e7754005968cba
Miles Whittaker <mjwhitta@gmail.com>
2017-04-07 13:32:25 -0400

    Find now honors the --ignore flag

--- lib/zoom/profile/find.rb
+++ lib/zoom/profile/find.rb
@@ -1,8 +1,22 @@
 class Zoom::Profile::Find < Zoom::Profile
     def initialize(n = nil, o = nil, f = nil, b = nil, a = nil)
+        a = "-print" if (a.nil? || a.empty?)
         f ||= ""
         o ||= "find"
         super(n, o, f, b, a)
         @taggable = true
+    end
+
+    def translate(from)
+        to = Array.new
+        from.each do |flag, value|
+            case flag
+            when "ignore"
+                value.each do |v|
+                    to.push("-name \"#{v}\" -prune -o")
+                end
+            end
+        end
+        return to.join(" ")
     end
 end
